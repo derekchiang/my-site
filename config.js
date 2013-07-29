f01b41cfd5554f68c6684a91cc05a5e054344caf
Derek Chiang <derekchiang93@gmail.com>
2013-07-28 22:43:51 -0700

    Init

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  indexPage: 'about',
+  baseView: 'index',
+  siteParams: {
+    title: "Derek's Site",
+    subtitle: 'Winner of this year\'s "Most Creative Site Title" award',
+  }
+}
